db6e1d6e72d1d71f06b6d379bd32a8d342401738
seohyuk1 <164006475+seohyuk1@users.noreply.github.com>
2025-04-29 20:44:11 +0900

    Delete find_id directory

--- find_id/constants/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color primaryColor = Color(0xFF222831);
-const Color borderColor = Color(0xFFDFDFDF);
-const Color backgroundColor = Color(0xFFEBEBEB);
